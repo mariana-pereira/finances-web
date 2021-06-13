7cfbbefb338cbbb943ed2d2d3dec3b231b1103a0
Mariana <marianna.22sp@gmail.com>
2021-06-13 09:23:55 -0300

    change js files to ts

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>finances</title>
+      </Head>
+
+      <main>
+       
+      </main>
+
+    </div>
+  )
+}
