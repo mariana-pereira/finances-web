476b26d30c31882c82f13f0211a4a666c50c8bdb
Mariana Pereira <mariprr22@gmail.com>
2024-11-28 11:48:21 -0300

    feat: create home layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,9 +1,15 @@
+import {
+  ChartNoAxesCombined,
+  CreditCard,
+  DollarSign,
+  House,
+  Landmark,
+  Settings,
+  Target,
+} from "lucide-react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Image from "next/image";
 import "../globals.css";
-
-import logo from '../../assets/finances.svg';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,17 +26,48 @@ export default function AuthLayout({
   return (
     <html lang="pt-BR" className="dark antialiased">
       <body className={inter.className}>
-        <div className="min-h-screen lg:grid lg:grid-cols-app dark:bg-zinc-900">
-          <div className="flex flex-col items-center">
-            <h1 className="to-zinc-900 text-7xl font-bold dark:text-zinc-100 mt-28 mb-16">
-              finances
-            </h1>
-            <Image src={logo} width={500} alt="" />
+        <div className="h-[100vh] w-[20vw] bg-blue-900 rounded-e-2xl flex flex-col items-center">
+          <div className="pb-8">
+            <div className="w-36 h-36 bg-zinc-500 rounded-full mt-16 mb-8"></div>
+            <strong className="text-3xl">Mariana Pereira</strong>
           </div>
-          <main className="max-w-[100vw] bg-blue-950 lg:col-start-2 lg:px-8 lg:pt-8">
-            {children}
-          </main>
+          <div className="w-[20vw] h-[80vh] bg-zinc-950 rounded-2xl py-9">
+            <div className="flex flex-row pl-12 h-16">
+              <House color="white" size={24} />
+              <strong className="ml-7 text-xl">Início</strong>
+            </div>
+            <div className="flex flex-row pl-12 h-16">
+              <Landmark color="white" size={24} />
+              <strong className="ml-7 text-xl">Contas</strong>
+            </div>
+            <div className="flex flex-row pl-12 h-16">
+              <CreditCard color="white" size={24} />
+              <strong className="ml-7 text-xl">Cartões</strong>
+            </div>
+            <div className="flex flex-row pl-12 h-16">
+              <Target color="white" size={24} />
+              <strong className="ml-7 text-xl">Objetivos</strong>
+            </div>
+            <div className="flex flex-row pl-12 h-16">
+              <ChartNoAxesCombined color="white" size={24} />
+              <strong className="ml-7 text-xl">Investimentos</strong>
+            </div>
+            <div className="flex flex-row pl-12 h-16">
+              <DollarSign color="white" size={24} />
+              <strong className="ml-7 text-xl">Budget</strong>
+            </div>
+
+            <div className={"border-t border-gray-300 my-0"}></div>
+
+            <div className="flex flex-row pl-12 h-16 pt-7">
+              <Settings color="white" size={24} />
+              <strong className="ml-7 text-xl">Configurações</strong>
+            </div>
+          </div>
         </div>
+        <main className="max-w-[100vw] lg:col-start-2 lg:px-8 lg:pt-8">
+          {children}
+        </main>
       </body>
     </html>
   );
